8442da0c0216a63c8a437b916c9207e3c472a19c
manu1199-max <manusenavirathna4@gmail.com>
2025-07-01 06:01:30 +0530

    Update play3.js

--- plugins/play3.js
+++ plugins/play3.js
@@ -1,5 +1,5 @@
-const config = require('../config');
-const { cmd } = require('../command');
+const config = require('../settings');
+const { cmd } = require('../lib/command');
 const { ytsearch } = require('@dark-yasiya/yt-dl.js');
 
 // MP4 video download
