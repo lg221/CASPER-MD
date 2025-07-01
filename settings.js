2d2282ce2524b051976eaf0542f446271818f954
manu1199-max <manusenavirathna4@gmail.com>
2025-07-01 06:06:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,6 +9,8 @@ SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=Vy02TQ7a#jK8yU6ghWQEZtvV8HSnTk1Z
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504', 
 PREFIX:  process.env.PREFIX || ['.'] ,
+JID: process.env.JID || "120363389486431984@newsletter",
+FOOTER: 'ලස්සන සිංදු✨☘️'
 LOGO: process.env.LOGO || `https://i.ibb.co/RMxmMc5/4568.jpg`,
 OWNER_REACT: process.env.OWNER_REACT || 'true',
 AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
