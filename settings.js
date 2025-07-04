5a6e2c69f9c46aae80c50257c034f18a531012aa
lg221 <liverboy233@gmail.com>
2025-07-04 13:10:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=Vy02TQ7a#jK8yU6ghWQEZtvV8HSnTk1ZX3Q_k61QCV1TZQQS5d7w,
+SESSION_ID: process.env.SESSION_ID || 'CASPER-MD=qkRghCZB#G-rCapahNMIUbB62M_jlPKUWT4uwMd87YC0RepddyEc,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504', 
 PREFIX:  process.env.PREFIX || ['.'] ,
