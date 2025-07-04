8546cfb583e926f0c128327a02fca773fdd4b374
lg221 <liverboy233@gmail.com>
2025-07-04 13:04:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,11 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'CASPER-MD=elQmmTSA#xyHGAJJgAlmR98i041v0guhH1Kj01A9CQ-TmNL0ZXi4',
+SESSION_ID: process.env.SESSION_ID || 'SAHAS-MD=Vy02TQ7a#jK8yU6ghWQEZtvV8HSnTk1ZX3Q_k61QCV1TZQQS5d7w,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504', 
-PREFIX:  process.env.PREFIX || ['.'] ,
-JID: process.env.JID || "120363389486431984@newsletter",
-FOOTER: '> ලස්සන සිංදු✨☘️ (𝕝𝕒𝕤𝕤𝕒𝕟𝕒 𝕤𝕚𝕟𝕕𝕦🪄)',
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504',
+PREFIX: process.env.PREFIX || ".",
+//PREFIX:  process.env.PREFIX || ['.'] ,
 LOGO: process.env.LOGO || `https://i.ibb.co/RMxmMc5/4568.jpg`,
 OWNER_REACT: process.env.OWNER_REACT || 'true',
 AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
