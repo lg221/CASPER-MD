846691491e8bceb5972f555cda0c215a6fceb1e8
lg221 <liverboy233@gmail.com>
2025-07-05 11:28:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,6 +9,8 @@ SESSION_ID: process.env.SESSION_ID || 'CASPER-MD=qkRghCZB#G-rCapahNMIUbB62M_jlPK
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 OWNER_NUMBER: process.env.OWNER_NUMBER || '94764582504', 
 PREFIX:  process.env.PREFIX || ['.'] ,
+FOOTER: '▫️ *React කරන්න ලමයෝ* 🇱🇰💗',
+JID process.env.JID || "120363417168743361@newsletter",
 LOGO: process.env.LOGO || `https://i.ibb.co/RMxmMc5/4568.jpg`,
 OWNER_REACT: process.env.OWNER_REACT || 'true',
 AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || "true",
